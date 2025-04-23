8a21d6b1e707bcbf27e016d349e71521b69cba14
Talha <tash192@aucklanduni.ac.nz>
2025-04-23 15:47:15 +1200

    feat: added persistent pc to PC block

--- vhdl-src/ProgramCounter.vhd
+++ vhdl-src/ProgramCounter.vhd
@@ -21,16 +21,19 @@ ARCHITECTURE behavior OF ProgramCounter IS
 BEGIN
 
     PROCESS (PC_SEL, Rx, RandomBlueLine, Adder_Out)
+    variable PC_var : natural range 0 to 5 := 0;
+
     BEGIN
         CASE PC_SEL IS
             WHEN "00" => -- PC = PC + 1
-                PC <= Rx + 1;
+                PC_var := Rx + 1;
             WHEN "01" => -- PC = RandomBlueLine
-                PC <= RandomBlueLine;
+                PC_var := RandomBlueLine;
             WHEN "10" => -- PC = Adder_Out
-                PC <= Adder_Out;
+                PC_var := Adder_Out;
             WHEN OTHERS => -- Default case
-                PC <= (OTHERS => '0'); -- Set to zero or some default value
+                PC_var := (OTHERS => '0'); -- Set to zero or some default value
         END CASE;
+        PC <= PC_var;
     END PROCESS;
 END behavior;
