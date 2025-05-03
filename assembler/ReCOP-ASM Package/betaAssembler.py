e671647a308000ca4d12e4070f980e7efc0a2feb
jste910 <jste910@aucklanduni.ac.nz>
2025-05-03 16:22:18 +1200

    Added assembler

---
+++ assembler/ReCOP-ASM Package/betaAssembler.py
@@ -0,0 +1,254 @@
+
+def binToHex(b): # b probably is a string
+    bLookup = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6', '0111': '7', '1000': '8', '1001': '9', '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'}
+    if len(b) % 4 != 0:
+        print("Error: Not enough bits")
+        return "XXXX"
+    h = ""
+    for i in range(0, len(b), 4):
+        sub = b[i:i+4]
+        if sub in bLookup:
+            h += bLookup[sub]
+    return h
+
+def hextobin(hx): # hex is a string
+    # convert hex to binary with a padding so it is always a multiple of 4
+    b=""
+    for c in hx:
+        b += bin(int(c, 16))[2:].zfill(4) # convert to binary and pad with 0s
+    return b
+def dectobin(dec):
+    # convert decimal to binary with a padding so it is always a multiple of 4
+    binary = f"{dec:b}"
+    pad_length = (4 - (len(binary) % 4)) % 4
+    return "0" * pad_length + binary
+
+def modifyParts(parts, lookup):
+    bLookup = {"0" : "0000","1" : "0001","2" : "0010","3" : "0011","4" : "0100","5" : "0101","6" : "0110","7" : "0111","8" : "1000","9" : "1001","10" : "1010","11" : "1011","12" : "1100","13" : "1101","14" : "1110","15" : "1111"} # dec to bin
+    instruction = "X"
+    address = "XX"
+    returnType = "X" # return type
+    returnString = "" # return string
+    rString = ""
+    # find the last
+    p = parts[-1]
+    if p[0] == "R": # Register
+        address = "11"
+        returnType = "RE"
+    elif p[0] == "#": # Immediate
+        address = "01"
+        returnType = "IM"
+    elif p[0] == "$": # Direct
+        address = "10"
+        returnType = "DI"
+    elif p in ['CLFZ', "CER", "CEOT", "SEOT", 'NOOP']: # Inherent
+        address = "00"
+        returnType = "IH"
+    else:
+        if p in lookup: # Lookup table
+            address = "01" # Immediate (idk)
+            returnType = "IM"
+        else:
+            address = "XX"
+
+    if parts[0] == "AND":
+        instruction = "001000"
+        # remaing parts
+        if parts[-1][0] == "#": # is an immediate value
+            # we have Rz, Rx #immediate
+            # Rz is 1 and Rx is 2
+            rString = bLookup[parts[1][1:]] + bLookup[parts[2][1:]] + dectobin(int(parts[-1][1:])).rjust(16, "0")
+        else: # We have a Register
+            # we have Rz Rz Rx
+            rString = bLookup[parts[-2][1:]] + bLookup[parts[-1][1:]]
+
+    if parts[0] == "OR":
+        instruction = "001100"
+        # remaing parts
+        if parts[-1][0] == "#": # is an immediate value
+            # we have Rz, Rx #immediate
+            # Rz is 1 and Rx is 2
+            rString = bLookup[parts[1][1:]] + bLookup[parts[2][1:]] + dectobin(int(parts[-1][1:])).rjust(16, "0")
+        else: # We have a Register
+            # we have Rz Rz Rx
+            rString = bLookup[parts[-2][1:]] + bLookup[parts[-1][1:]]
+    if parts[0] == "ADD":
+        instruction = "111000"
+        # remaing parts
+        if parts[-1][0] == "#": # is an immediate value
+            # we have Rz, Rx #immediate
+            # Rz is 1 and Rx is 2
+            rString = bLookup[parts[1][1:]] + bLookup[parts[2][1:]] + dectobin(int(parts[-1][1:])).rjust(16, "0")
+        else: # We have a Register
+            # we have Rz Rz Rx
+            rString = bLookup[parts[-2][1:]] + bLookup[parts[-1][1:]]
+    if parts[0] == "SUBV":
+        instruction = "000011"
+        rString = bLookup[parts[1][1:]] + bLookup[parts[2][1:]] + dectobin(int(parts[-1][1:])).rjust(16, "0")
+
+    if parts[0] == "SUB":
+        instruction = "000100"
+        rString = bLookup[parts[1][1:]] + dectobin(int(parts[-1][1:])).rjust(20, "0")
+    if parts[0] == "LDR":
+        instruction = "000000"
+        # remaing parts
+        if parts[-1][0] == "#": # is an immediate value
+            # we have Rz #immediate
+            # Rz is 1 and Rx is 2
+            rString = bLookup[parts[1][1:]] + dectobin(int(parts[-1][1:])).rjust(20, "0")
+        elif parts[-1][0] == "$": # is a direct value
+            rString = bLookup[parts[1][1:]] + hextobin((parts[-1][1:])).rjust(20, "0")
+        else: # We have a Register
+            # we have Rz Rx
+            rString = bLookup[parts[-2][1:]] + bLookup[parts[-1][1:]]
+    if parts[0] == "STR":
+        instruction = "000010"
+        if parts[-1][0] == "#": # is an immediate value
+            # we have Rz #immediate
+            # Rz is 1 and Rx is 2
+            rString = bLookup[parts[1][1:]] + dectobin(int(parts[-1][1:])).rjust(20, "0")
+        elif parts[-1][0] == "$": # is a direct value
+            rString = "0000" + bLookup[parts[1][1:]] + hextobin((parts[-1][1:])).rjust(16, "0")
+        else: # We have a Register
+            # we have Rz Rx
+            rString = bLookup[parts[-2][1:]] + bLookup[parts[-1][1:]]
+    if parts[0] == "JMP":
+        instruction = "011000"
+        if parts[-1][0].islower(): # is an immediate value
+            # we have Rz #immediate
+            rString = hextobin(str(lookup[parts[-1]])).rjust(24, "0")
+        else: # register
+            rString = "0000" + bLookup[parts[-1][1:]]
+    if parts[0] == "PRESENT":
+        instruction = "011100"
+        rString = bLookup[parts[1][1:]] + hextobin(str(lookup[parts[-1]])).rjust(20, "0")
+    if parts[0] == "DATACALL":
+        instruction = "101000"
+        rString = "0000" + bLookup[parts[1][1:]]
+        if parts[-1][0] == "#":
+            instruction = "101001"
+            rString += dectobin(int(parts[-1][1:])).rjust(16, "0")
+    if parts[0] == "SZ":
+        instruction = "010100"
+        rString = hextobin(str(lookup[parts[-1]])).rjust(24, "0")
+    if parts[0] == "CLFZ":
+        instruction = "010000"
+        rString = "00000000"
+    if parts[0] == "CER":
+        instruction = "111100"
+        rString = "00000000"
+    if parts[0] == "CEOT":
+        instruction = "111110"
+        rString = "00000000"
+    if parts[0] == "SEOT":
+        instruction = "111111"
+        rString = "00000000"
+    if parts[0] == "LER":
+        instruction = "110110"
+        rString = bLookup[parts[1][1:]] + "0000"
+    if parts[0] == "SSVOP":
+        instruction = "111011"
+        rString = "0000" + bLookup[parts[1][1:]]
+    if parts[0] == "LSIP":
+        instruction = "110111"
+        rString = bLookup[parts[1][1:]] + "0000"
+    if parts[0] == "SSOP":
+        instruction = "111010"
+        rString = "0000" + bLookup[parts[1][1:]]
+    if parts[0] == "NOOP":
+        instruction = "110100"
+        rString = "00000000"
+    if parts[0] == "MAX":
+        instruction = "011110"
+        rString = bLookup[parts[1][1:]] + dectobin(int(parts[-1][1:])).rjust(20, "0")
+    if parts[0] == "STRPC":
+        instruction = "011101"
+        rString = hextobin(parts[-1][1:]).rjust(24, "0")
+
+    # get the hex equivalent of the binary string
+    ai = binToHex(address + instruction) + binToHex(rString)
+
+    print(parts, ai)
+    return returnType, ai
+
+filePath = "assembler/ReCOP-ASM Package/"
+# f = filePath + "gpt.asm"
+f = filePath + "test3.asm"
+
+
+lookup = {}
+finallines = []
+with open(f, "r") as file:
+    lines = file.readlines()
+    idx = 0
+    for line in (lines):
+        # we need to filter out the commends, empty lines and tabs or spaces
+        l = line.strip()
+        if l == "" or l[0] == ";" or l == "ENDPROG" or l == "END": # I hope l == "\n" is not needed
+            continue # we don't care about endprog
+        if l.count(";") > 0: # if there is a comment, we need to remove it
+            l = l.split(";")[0] # remove the comment
+        # if the line has a lowecase first letter, add it to the lookup table
+        if l[0].islower():
+            lookup[f"{l}"] = idx
+            continue
+        print(f"Line {idx} : {l}")
+        finallines.append(l) # add to the next pass
+        idx+= 1
+
+print(f"Lookup table: {lookup}")
+o = []
+tList = [] # type list
+for line in finallines: # second pass
+    parts = line.strip().split()
+    tpe, output = modifyParts(parts, lookup)
+
+    # print(f"{parts} -> {output} || {len(output)} bits")
+    o.append(output)
+    tList.append(tpe) # add to the type list
+
+
+
+# writing the .mif file
+with open(f"{f[:-4]}.mif", "w") as file:
+    file.write("WIDTH = 16;\n")
+    file.write("DEPTH = 1024;\n\n") # double newline
+    file.write("ADDRESS_RADIX = HEX;\n")
+    file.write("DATA_RADIX = HEX;\n\n") # double newline
+    file.write("CONTENT\n\tBEGIN\n")
+    file.write("\t\t[00..3FF]: FFFF;\n")
+    c = 0
+    for i in range(len(finallines)):
+        if len(o[i]) == 4:
+            # we have a 4 bit instruction, we need to add the address
+            file.write(f"\t\t{c:X}\t:{o[i]};\n")
+        else:
+            # we have a 8 bit instruction
+            file.write(f"\t\t{c:X}\t:{o[i][:4]};\n")
+            c += 1
+            # we need to add the next part
+            file.write(f"\t\t{c:X}\t:{o[i][4:]};\n")
+        c += 1
+
+    file.write("\tEND;\n")
+
+
+# we write a comparator to check the output
+# f1 = filePath + "rawOutput.mif"
+f1 = "DE1-FPGA\program.mif"
+f2 = f[:-4] + ".mif"
+print(f"Comparing {f1} and {f2}")
+
+fileright = open(f1, "r")
+filecheck = open(f2, "r")
+
+filerightlines = fileright.readlines()
+filechecklines = filecheck.readlines()
+fileright.close()
+filecheck.close()
+
+for i in range(len(filerightlines)):
+    if filerightlines[i] != filechecklines[i]:
+        print(f"Index: |{i} Expected: {filerightlines[i]}|", end = " ")
+        print(f"Got: |{filechecklines[i]}|")
+
---
+++ assembler/ReCOP-ASM Package/gpt.asm
@@ -0,0 +1,54 @@
+start
+    ; Load values
+    LDR R1 #10          ; Immediate load
+    LDR R2 R1           ; Register indirect load
+    LDR R3 $100         ; Memory load
+
+    ; Store values
+    STR R1 #20          ; Immediate store
+    STR R2 R1           ; Register indirect store
+    STR R3 $200         ; Memory store
+
+    ; Logical operations
+    AND R4 R1 #15       ; R4 = R1 AND 15
+    AND R4 R4 R2        ; R4 = R4 AND R2
+    OR R5 R1 #3         ; R5 = R1 OR 3
+    OR R5 R5 R2         ; R5 = R5 OR R2
+
+    ; Arithmetic
+    ADD R6 R1 #5        ; R6 = R1 + 5
+    ADD R6 R6 R2        ; R6 = R6 + R2
+    SUBV R7 R6 #2       ; R7 = R6 - 2 with overflow check
+    SUB R7 #1           ; R7 = R7 - 1
+
+    ; Jumps
+    JMP loopstart             ; Jump to address 50
+loopstart
+    JMP R1              ; Jump to address in R1
+
+    ; ; Output
+    PRESENT R1 presjmp       ; Display R1 in format 1
+presjmp
+
+    ; ; Data and control flow
+    DATACALL R1         ; Data call using R1
+    DATACALL R2 #5      ; Data call with parameter
+    SZ szjmp               ; Set if zero, compare with 3
+szjmp
+    CLFZ                ; Clear flag zero
+    CER                 ; Clear error register
+    CEOT                ; Clear end of transmission
+    SEOT                ; Set end of transmission
+
+    ; System and logic
+    LER R3              ; Load error register into R3
+    SSVOP R1            ; Save system value
+    LSIP R2             ; Load system input pointer
+    SSOP R3             ; Save system operation
+
+    ; Miscellaneous
+    NOOP                ; Do nothing
+    MAX R4 #99          ; Max compare with 99
+    STRPC $300          ; Store PC to memory
+ENDPROG
+END
